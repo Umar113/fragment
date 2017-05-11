b4783e1ba5fc60c9dc29c0be8a0c0a4824229ad1
Umar113 <taufeequeahmad113@gmail.com>
2017-05-11 16:14:59 +0530

    My Files

---
+++ app/src/main/java/www/ssitinfotech/com/uploadimage/Constants.java
@@ -0,0 +1,9 @@
+package www.ssitinfotech.com.uploadimage;
+
+/**
+ * Created by Android on 09-03-2017.
+ */
+public class Constants {
+    public static final String UPLOAD_URL = "http://192.168.0.7/AndroidImageUpload/upload.php";
+    public static final String IMAGES_URL = "http://192.168.94.1/AndroidImageUpload/getImages.php";
+}
